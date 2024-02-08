436d22b8f1994accc4d9ede294af5a18b961201f
Will Jarrard <wejarrard@gmail.com>
2024-02-08 15:27:36 -0800

    Added another highlight to steps

--- src/lib/steps.js
+++ src/lib/steps.js
@@ -6,8 +6,15 @@ export const steps = [
         description: "Pretrained and finetuned a <strong class='text-teal-600'>foundational model</strong> on <strong class='text-teal-600'>Amazon Sagemaker</strong> that incorporated epigenetic information alongside sequence information. When pretrained on TF binding data, the model achieved almost <strong class='text-teal-600'>90% accuracy</strong>. (In progress)."
     },
     {
+        name: "Digitization of Medical Records",
+        icon: "fa-solid fa-hospital-user",
+        href: "/blog/handwritten-classification",
+        description: "I developed a cutting-edge method for extracting crucial information, like drug names, from paper medical sheets. By utilizing <strong class='text-teal-600'>YOLO</strong> for segmentation and <strong class='text-teal-600'>LSTMs</strong> to capture the sequential nature of drugs, my method achieved impressive accuracy rates. This project has the potential to streamline the medical record-keeping process and <strong class='text-teal-600'>improve</strong> patient care."
+    },
+    {
         name: "My Portfolio",
         icon: "fa-solid fa-house",
+        href: "https://github.com/wejarrard/portfolio",
         description: "My portfolio is a website written in <strong class='text-teal-600'> Sveltekit </strong> using <strong class='text-teal-600'>Tailwind</strong> for styling. It is hosted on <strong class='text-teal-600'>Vercel</strong> and is a showcase of my ability to learn and use new technologies on my own."
     },
 ];
