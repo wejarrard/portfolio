bc56f02b688d582934b3392906e2fde21fc20ac0
Will Jarrard <wejarrard@gmail.com>
2024-05-31 09:22:42 -0700

    Updates

--- src/lib/steps.js
+++ src/lib/steps.js
@@ -12,7 +12,7 @@ export const steps = [
         description: "Developed a method for extracting crucial information, like drug names, from paper medical sheets. By utilizing YOLO for segmentation and LSTMs to capture the sequential nature of drugs, my method achieved impressive accuracy rates. This project has the potential to streamline the medical record-keeping process and improve patient care."
     },
     {
-        name: "UTX ",
+        name: "UTX Condensation",
         icon: "fa-solid fa-dna",
         href: "https://www.nature.com/articles/s41586-021-03903-7",
         description: "Studied how UTX, a histone demethylase and tumor suppressor, relies on phase separation into liquid condensates for its chromatin-regulatory activity. Loss of its intrinsically disordered region (cIDR), common in cancer mutations, disrupts this process, impairing tumor suppression and chromatin interactions."
