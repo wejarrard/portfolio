4c322ec8ac372edc651f1da98f6665d828b4f8d6
Will Jarrard <wejarrard@gmail.com>
2024-06-05 22:05:31 -0700

    wording

--- src/lib/steps.js
+++ src/lib/steps.js
@@ -14,9 +14,10 @@ export const steps = [
         description: "Developed a method for extracting crucial information, like drug names, from paper medical sheets. By utilizing YOLO for segmentation and LSTMs to capture the sequential nature of drugs, out method achieved impressive accuracy rates. This project has the potential to streamline the medical record-keeping process and improve patient care."
     },
     {
-        name: "Transcription Factor Colocalization Deep Learning Model", 
+        name: "Investigating Transcription Factor Colocalization in RB1/TP53 Knockout Prostate Cancer", 
         icon: "fa-solid fa-network-wired",
+        paperName: "In progress",
         href: "https://github.com/wejarrard/tf-binding",
-        description: "Currently studying transcription factor colocalization in prostate cancer using deep learning (In progress)."
+        description: "Investigating transcription factor colocalization in prostate cancer using an innovative deep learning model to understand the increased aggressiveness of RB1/TP53 double knockout cases. This project is currently in progress."
     },
 ];
